a0d7e2ed5546f7692ec3f0a27ca35abaa5b8428e
Andres Velasco <velasco.andrs@gmail.com>
2026-07-14 12:02:15 -0500

    feat: add InvoiceService for invoice management, enhance DbClient with SQL execution methods

--- src/services/services.layer.ts
+++ src/services/services.layer.ts
@@ -1,5 +1,6 @@
 import { Layer } from 'effect'
+import { InvoiceService } from './invoice.service.js'
 import { SriService } from './sri.service.js'
 
 // HttpClient se provee globalmente desde el MainLayer.
-export const ServicesLayer = Layer.mergeAll(SriService.Default)
+export const ServicesLayer = Layer.mergeAll(SriService.Default, InvoiceService.Default)
